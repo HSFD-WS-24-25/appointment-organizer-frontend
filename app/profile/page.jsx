83dbab6db7f4e94fb04ec7150eea977cab6feb8e
János Euler <janos-conrad.euler@informatik.hs-fulda.de>
2024-11-18 19:30:07 +0100

    minor addition to profile

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0/client';
+import {Avatar} from "@mui/material";
 
 //import ErrorMessage from '../../components/ErrorMessage';
 
@@ -17,16 +18,12 @@ function Profile() {
           className="flex flex-col md:flex-row items-center mb-5 text-center md:text-left"
           data-testid="profile"
         >
-          <div className="md:w-1/6">
-            <img
+          <Avatar
+              alt="Remy Sharp"
               src={user.picture}
-              alt="Profile"
-              className="rounded-full w-24 h-24 md:w-32 md:h-32 mb-3 md:mb-0"
-              decode="async"
-              data-testid="profile-picture"
-            />
-          </div>
-          <div className="md:w-5/6">
+              sx={{ width: 90, height: 90 }}
+          />
+          <div className="md:w-5/6 ml-2">
             <h2 data-testid="profile-name" className="text-2xl font-semibold">{user.name}</h2>
             <p className="text-muted text-gray-500" data-testid="profile-email">
               {user.email}
