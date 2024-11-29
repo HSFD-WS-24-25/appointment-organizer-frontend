2ece86b948cb48ed59407e4817329113fb08421a
János Euler <janos-conrad.euler@informatik.hs-fulda.de>
2024-11-29 12:15:58 +0100

    made the backend url available in the Env-Vars

--- app/lib/useFetchApiData.jsx
+++ app/lib/useFetchApiData.jsx
@@ -3,9 +3,10 @@ import {useEffect, useState} from "react";
 export function useFetchApiData(user, path, method) {
     const [data, setData] = useState([]);
     const [error, setError] = useState(null);
-
+    const backend_url = process.env.NEXT_PUBLIC_BACKEND_URL
     useEffect(() => {
         const fetchProtectedData = async () => {
+            console.log(backend_url)
             try {
                 if (!user) {
                     console.log('User is not logged in');
@@ -19,7 +20,8 @@ export function useFetchApiData(user, path, method) {
                 console.log('Token:', accessToken);
 
                 // Use the token to access the protected backend route
-                const response = await fetch(`http://localhost:3001/${path}`, {
+
+                const response = await fetch(`${backend_url}${path}`, {
                     method, // Directly use the method string
                     headers: {
                         Authorization: `Bearer ${accessToken}`,
